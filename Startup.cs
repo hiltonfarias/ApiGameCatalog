15bc142506b24269a44cb9d21fa69b038eefab74
hiltonfarias <hilton.farias.da.silva@gmail.com>
2021-10-27 17:39:27 -0300

    implementando GameRepository

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiGameCatalog.Repositories;
+using ApiGameCatalog.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,8 @@ namespace ApiGameCatalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IGameService, GameService>();
+            services.AddScoped<IGameRepository, GameRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
